9425487bcdc31cce38fbc741b6ac164753b7d53c
Mojtaba Hassani <96564291+BigMoj@users.noreply.github.com>
2023-01-26 20:56:00 +0330

    writed english to morse code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from data import morse_code
+
+def translate(text):
+    morse_text = ""
+    for char in text:
+        char = char.upper()
+        if char in morse_code:
+            morse_text += morse_code[char] + " "
+        else:
+            morse_text += "       "
+    return morse_text
+
+print(translate(input("please insert your text:\n")))
+
 
